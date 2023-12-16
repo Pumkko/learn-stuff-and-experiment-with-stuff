218e3808930b0c26a76f6251d9e4581060f5091f
Puma <c.coque@hotmail.fr>
2023-12-16 08:44:48 -0500

    experiment with onPush

--- relearn-angular/README.md
+++ relearn-angular/README.md
@@ -2,6 +2,13 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.1.
 
+## What did I Learn
+### OnPush 
+Played with OnPush a bit and learn the following thing : 
+ - If component A is a parent of component B, and B has an input of a complex type, when A updates B might not see the changes if the reference to the complex type in A is the same
+ - If A and B are not related to each other and are sharing data through a service, then that service can use signal() so that when A calls the service to write a new value, B can be rerendered
+ Kept the code in playWithOnPush if i ever want to play with it again
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
--- relearn-angular/src/app/app.component.ts
+++ relearn-angular/src/app/app.component.ts
@@ -3,19 +3,22 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ChildComponent, Counter } from './playWithOnPush/child/child.component';
+import { CounterService } from './playWithOnPush/counter.service';
+import { ParentComponent } from './playWithOnPush/parent/parent.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, ParentComponent],
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'relearn-angular';
-  
 
-  ngOnInit(){
+
+  ngOnInit() {
     initFlowbite()
   }
 
